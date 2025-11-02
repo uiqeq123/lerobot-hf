e6ab61c612b605a3f56abfe8dc6ce01cd4b558c4
root <service@uiqeq.com>
2025-11-02 11:45:03 +0800

    --add rosmaster class

---
+++ src/lerobot/robots/rosmaster/config_rosmaster.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+from lerobot.cameras import CameraConfig
+from lerobot.cameras.opencv import OpenCVCameraConfig
+from lerobot.robots import RobotConfig
+
+
+@RobotConfig.register_subclass("rosmaster")
+@dataclass
+class RosmasterRobotConfig(RobotConfig):
+    port: str
+    #Cameras tutorial to understand how to detect and add your camera.
+    cameras: dict[str, CameraConfig] = field(
+        default_factory={
+            "cam_1": OpenCVCameraConfig(
+                index_or_path=2,
+                fps=30,
+                width=480,
+                height=640,
+            ),
+        }
+    )
